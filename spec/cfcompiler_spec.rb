32975b6d9b0ff03e1b377cf5efcc49f4058724c6
Bas van der Graaf <bvdgraaf@gmail.com>
2019-05-23 22:08:33 +0200

    extra tests json output

--- spec/cfcompiler_spec.rb
+++ spec/cfcompiler_spec.rb
@@ -3,7 +3,7 @@
 require './cfcompiler.rb'
 
 describe 'cfcompiler' do
-  it 'check output string' do
+  it 'check output #1' do
     options = {
       instances: 1,
       instance_type: 't2.micro',
@@ -13,5 +13,39 @@ describe 'cfcompiler' do
     template = json.to_json
     parsed_template = JSON.parse(template)
     expect(parsed_template['AWSTemplateFormatVersion']).to eql('2010-09-09')
+    expect(template).to include('0.0.0.0/0')
+    expect(template).to include('t2.micro')
+    expect(parsed_template).to have_key('Outputs')
+    if parsed_template.key?('Outputs')
+      expect(parsed_template['Outputs']).to have_key('PublicIP')
+    end
+    expect(parsed_template).to have_key('Resources')
+    if parsed_template.key?('Resources')
+      expect(parsed_template['Resources']).to have_key('EC2Instance')
+      expect(parsed_template['Resources']).to have_key('InstanceSecurityGroup')
+    end
+  end
+  it 'check output #2' do
+    options = {
+      instances: 2,
+      instance_type: 't2.small',
+      allow_ssh_from: '37.17.210.74/32'
+    }
+    json = CfCompiler.compile(options)
+    template = json.to_json
+    parsed_template = JSON.parse(template)
+    expect(parsed_template['AWSTemplateFormatVersion']).to eql('2010-09-09')
+    expect(template).to include('37.17.210.74/32')
+    expect(template).to include('t2.small')
+    expect(parsed_template).to have_key('Outputs')
+    if parsed_template.key?('Outputs')
+      expect(parsed_template['Outputs']).to have_key('PublicIP')
+    end
+    expect(parsed_template).to have_key('Resources')
+    if parsed_template.key?('Resources')
+      expect(parsed_template['Resources']).to have_key('EC2Instance')
+      expect(parsed_template['Resources']).to have_key('EC2Instance2')
+      expect(parsed_template['Resources']).to have_key('InstanceSecurityGroup')
+    end
   end
 end
