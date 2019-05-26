7579a658adfa3e17d070b6a08e4694136bac0d24
Bas van der Graaf <bvdgraaf@gmail.com>
2019-05-26 20:50:40 +0200

    check with rubocop

--- assignment.rb
+++ assignment.rb
@@ -25,34 +25,36 @@ else
   )
 end
 
+# TODO: verplaatsen naar aparte class?!
+def check_stack(cf_client, stack_name)
+  resp_describe = cf_client.describe_stacks(
+    stack_name: stack_name
+  )
+  if resp_describe.stacks[0].stack_status != 'CREATE_IN_PROGRESS'
+    if resp_describe.stacks[0].stack_status == 'CREATE_COMPLETE'
+      puts 'Stack creation successful, PublicIP:'
+      puts resp_describe.stacks[0].outputs[0].output_value
+    else
+      puts 'Stack creation failed with status:'
+      puts resp_describe.stacks[0].stack_status
+    end
+    return false
+  else
+    puts resp_describe.stacks[0].stack_status
+    return true
+  end
+end
+
 # validate JSON with AWS and create stack if create_stack is true
 if options[:create_stack].to_s == 'true'
   resp_validate = cf_client.validate_template(template_body: json.to_json)
   if resp_validate.successful?
     puts 'Cloudformation JSON valid, creating stack:'
-    resp_create = cf_client.create_stack(
+    cf_client.create_stack(
       stack_name: options[:stack_name],
       template_body: json.to_json
     )
-    check_stack = true
-    while check_stack do
-      resp_describe = cf_client.describe_stacks(
-        stack_name: options[:stack_name]
-      )
-      if resp_describe.stacks[0].stack_status != 'CREATE_IN_PROGRESS'
-        if resp_describe.stacks[0].stack_status == 'CREATE_COMPLETE'
-          puts 'Stack creation successful, PublicIP:'
-          puts resp_describe.stacks[0].outputs[0].output_value
-        else resp_describe.stacks[0].stack_status != 'CREATE_IN_PROGRESS'
-          puts 'Stack creation failed with status:'
-          puts resp_describe.stacks[0].stack_status
-        end
-        check_stack = false
-      else
-        puts resp_describe.stacks[0].stack_status
-      end
-      sleep 2
-    end
+    sleep 2 while check_stack(cf_client, options[:stack_name])
   elsif !resp_validate.successful?
     puts 'Cloudformation JSON template invalid'
   end
